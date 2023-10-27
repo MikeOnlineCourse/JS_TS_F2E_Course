c777fcfbccb09e34aa924713f8aa107a1f044f22
MikeCheng1208 <a3804430@hotmail.com>
2023-10-27 22:26:36 +0800

    fix: fix file name

--- example/21-TypeScript入門/vite-ts-project/src/ts/main.ts
+++ example/21-TypeScript入門/vite-ts-project/src/ts/main.ts
@@ -1,5 +1,5 @@
-import { titleRef, inpTxtRef, sendRef } from "./4-DOM.ts";
-import "./9-TypeAssertion.ts";
+import { titleRef, inpTxtRef, sendRef } from "./04-DOM.ts";
+import "./09-TypeAssertion.ts";
 import "./10-TypeFunction.ts";
 import "./11-Generics.ts";
 import "./12-Asynchronous.ts";
