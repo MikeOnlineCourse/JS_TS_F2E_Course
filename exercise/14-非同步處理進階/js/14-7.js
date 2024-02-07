67da73ecacc4b5c0f48fca9232554b83886e6c3e
MikeCheng1208 <a3804430@hotmail.com>
2024-02-07 16:21:23 +0800

    fix: demo code

--- exercise/14-非同步處理進階/js/14-7.js
+++ exercise/14-非同步處理進階/js/14-7.js
@@ -8,15 +8,22 @@
  *  terms
  */
 
+// input
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const email = document.querySelector("#email");
 const age = document.querySelector("#age");
+const sex = document.querySelectorAll("input[name='sex']");
+const terms = document.querySelector("#terms");
+
+// error message
 const usernameError = document.querySelector(".username-error");
 const passwordError = document.querySelector(".password-error");
 const emailError = document.querySelector(".email-error");
-const sex = document.querySelectorAll("input[name='sex']");
-const terms = document.querySelector("#terms");
+
+// 送出按鈕
 const btn = document.querySelector(".btn");
+// 註冊的頁面
 const loginPage = document.querySelector("#login-page");
+// 註冊完成頁面
 const successPage = document.querySelector("#success-page");
