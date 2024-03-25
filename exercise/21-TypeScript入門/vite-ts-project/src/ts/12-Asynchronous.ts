559a50aa9f8dbf79c47b15ff4d59c7b8296d3c2d
mike <a3804430@hotmail.com>
2024-03-25 17:00:20 +0800

    fix: demo code

--- exercise/21-TypeScript入門/vite-ts-project/src/ts/12-Asynchronous.ts
+++ exercise/21-TypeScript入門/vite-ts-project/src/ts/12-Asynchronous.ts
@@ -1,3 +1,30 @@
 // 非同步(Asynchronous)函式
+import axios from "axios";
 
-// https://vue-lessons-api.vercel.app/photo/list
+async function apiRequest(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return {
+    status: response.status,
+    data
+  }
+}
+
+apiRequest("https://vue-lessons-api.vercel.app/photo/list").then((res) => {
+  console.log("API then=>", res);
+});
+
+const res = await apiRequest("https://vue-lessons-api.vercel.app/photo/list")
+console.log("API await=>", res);
+
+
+axios.get("https://vue-lessons-api.vercel.app/photo/list").then((res) => {
+  console.log("axios=>", res);
+});
+
+function apiGetPhotoList() {
+  return axios.get("https://vue-lessons-api.vercel.app/photo/list");
+}
+
+const resPhoto = await apiGetPhotoList();
+console.log("photo list=>", resPhoto);
