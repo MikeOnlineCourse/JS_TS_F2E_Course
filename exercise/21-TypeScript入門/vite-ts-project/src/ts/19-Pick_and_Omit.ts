f0243fecf4c301891092ad26ef4ff44e14e1fa3c
MikeCheng1208 <a3804430@hotmail.com>
2024-03-26 20:54:00 +0800

    fix: demo code

--- exercise/21-TypeScript入門/vite-ts-project/src/ts/19-Pick_and_Omit.ts
+++ exercise/21-TypeScript入門/vite-ts-project/src/ts/19-Pick_and_Omit.ts
@@ -9,5 +9,16 @@ type TUserAllType = {
 };
 
 // 可以使用 Pick 來挑選出需要的屬性，另外組成新的 type
+const userBaseType = {
+  name: "Mike",
+  userAddress: "Taiwan",
+  jobTitle: "Frontend Engineer",
+};
 
 // 使用 Omit 來挑選出不需要的屬性，另外組成新的 type
+const userJobType = {
+  companyName: "TheCodingPro",
+  jobTitle: "Teacher",
+  seniority: 3,
+  companyAddress: "台灣台北市",
+};
--- exercise/21-TypeScript入門/vite-ts-project/src/ts/20-Extract_and_Exclude.ts
+++ exercise/21-TypeScript入門/vite-ts-project/src/ts/20-Extract_and_Exclude.ts
@@ -2,7 +2,8 @@ type TButtonKeyAttr = "create" | "edit" | "success" | "error";
 type TButtonValue = "新增" | "修改" | "完成" | "失敗";
 
 //  Extract 聯合類型 "選取" 指定內容，另外組成新的 type
-
+const successUseBtn = ["create", "success"];
 
 // Exclude 聯合類型 "排除" 指定內容，另外組成新的 type
+const errorUseBtn = ["edit", "error"];
 
