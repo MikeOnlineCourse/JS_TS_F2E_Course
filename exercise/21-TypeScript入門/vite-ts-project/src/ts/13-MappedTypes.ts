0f20dbea922447fb0bce62e9b68ea41d86f64cb3
mike <a3804430@hotmail.com>
2024-03-26 15:48:58 +0800

    fix: demo code

--- exercise/21-TypeScript入門/vite-ts-project/src/ts/13-MappedTypes.ts
+++ exercise/21-TypeScript入門/vite-ts-project/src/ts/13-MappedTypes.ts
@@ -1,1 +1,28 @@
 // 使用映射型別 (Mapped Types)
+
+const lolState = {
+    Offline: "離線",
+    Online: "在線",
+    leave: "離開",
+    nostate: "不存在"
+}
+
+
+const lolState2 = {
+    Offline: "離線",
+    Online: "在線",
+    leave: "離開",
+    nostate: "不存在"
+}
+
+
+// ----------------------------------------------------------
+
+
+
+const lolStateMap = {
+    Offline: "離線",
+    Online: "在線",
+    leave: "離開",
+    nostate: "不存在"
+};
--- exercise/21-TypeScript入門/vite-ts-project/src/ts/14-Record.ts
+++ exercise/21-TypeScript入門/vite-ts-project/src/ts/14-Record.ts
@@ -1,2 +1,28 @@
 type TBtnKeyAttr = "create" | "edit" | "success"| "error";
 type TBtnValue = "新增" | "修改" | "完成" | "失敗";
+
+const BtnStatusMap = {
+    create: "新增",
+    edit: "修改",
+    success: "完成",
+    error: "失敗"
+};
+  
+
+// ----------------------------------------------------------
+
+
+const subscribersUserInfo = {
+    mike: {
+      age: 18,
+      address: "桃園"
+    },
+    jacky: {
+      age: 20,
+      address: "台北"
+    },
+    andy: {
+      age: 22,
+      address: "台中"
+    }
+};
